48b782f2d78f4ccb79ea6c8b8196e7f5f1e43ad5
PabloC2A <116039498+PabloC2A@users.noreply.github.com>
2025-01-08 19:25:10 -0500

    actualizacion rutas

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 // Ruta Publica: Login
-router.post('/auth', authController.login);
+router.post('/login', authController.login);
 
 module.exports = router;
